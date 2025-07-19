46ba6c79d93d0aef15c4f7ed5512392ff43ac245
Goth-DongHaiTang <m15178682641@163.com>
2025-07-19 23:19:59 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request, env) {
+    const key = url.pathname.slice(1);
+    await env.MY_BUCKET.put(key, request.body);
+    return new Response(`Put ${key} successfully!`);
+  },
+};
