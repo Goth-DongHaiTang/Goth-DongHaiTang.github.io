52e6eaeb7f18cf209f435f6a67b06c3c78866309
Goth-DongHaiTang <m15178682641@163.com>
2025-07-19 23:34:37 +0800

    p

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env) {
-    const key = url.pathname.slice(1);
+    const key = new URL(request.url).pathname.slice(1);
     await env.MY_BUCKET.put(key, request.body);
     return new Response(`Put ${key} successfully!`);
   },
