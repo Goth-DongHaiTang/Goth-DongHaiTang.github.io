d460f010a123a106d496aabd0ee3d1195c6506b8
Goth-DongHaiTang <m15178682641@163.com>
2025-07-20 18:18:13 +0800

    js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-alert("Ciallo～(∠・ω< )⌒★");
+"use strict"; 
+// 上面的意思，启用现代的JS
+alert("Ciallo～");
+alert("(∠・ω< )⌒★");
+
+const userName = prompt("请输入你的名称",["(名称)"]);
+if (userName=NaN || userName === null || userName.trim() === "") {
+    alert("请输入你的名称！");
+    return;
+}
+alert(`欢迎你，${userName}！`);
+
+/*  
+let result = confirm("你喜欢这个网站吗？");
+if (result) {
+    alert("谢谢你的支持！");
+}
+else {
+    alert("没关系，我们会继续努力的！");
+}
+    */
+
+  
