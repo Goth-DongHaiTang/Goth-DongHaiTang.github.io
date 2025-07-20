666c880ef34f5f3d4fc8349ecd3d6b12d9b872a7
Goth-DongHaiTang <m15178682641@163.com>
2025-07-20 22:28:12 +0800

    l

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-"use strict"; 
-// 上面的意思，启用现代的JS
 alert("Ciallo～");
 alert("(∠・ω< )⌒★");
 
-const userName = prompt("请输入你的名称",["(名称)"]);
-alert(`欢迎你，${userName}！`);
 
 /*  
 let result = confirm("你喜欢这个网站吗？");
