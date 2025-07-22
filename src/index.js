9c3dc58b0d80caf09dba5be49fb6d6e633cfde46
Goth-DongHaiTang <m15178682641@163.com>
2025-07-22 17:01:33 +0800

    js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
-const name = prompt("请输入你的名字✅：",["宁宁"]);
-let borwerName = navigator.userAgent;
-let systemName = navigator.platform;
+const name = prompt("请输入你的名字✅：",[""]);
+const borwerName = getBrowserName(navigator.userAgent);
+const systemName = navigator.platform;
 alert(`Ciallo~(∠・ω< )⌒★, ${name} ! 🎉🎉🎉。\n 你当前的浏览器是:${borwerName},\n系统是:${systemName}`);
 
 if (name=="宁宁"){
     alert("你已登录！");
-    confirm("是否跳转到彩蛋界面？")
+    let bool = confirm("是否跳转到彩蛋界面？")
+    if (bool==true){
+        
     location.href = "./admin.html";
 }
+    else{
+        alert("已返回主页")
+
+    }
+}
 
 
-/*  
-let result = confirm("你喜欢这个网站吗？");
-if (result) {
-    alert("谢谢你的支持！");
+
+
+
+
+
+
+
+
+
+
+function getBrowserName(userAgent) {
+  // 此处顺序很重要，并且对于未列出的浏览器，这可能会报错。
+
+  if (userAgent.includes("Firefox")) {
+    // "Mozilla/5.0 (X11; Linux i686; rv:104.0) Gecko/20100101 Firefox/104.0"
+    return "Mozilla Firefox";
+  } else if (userAgent.includes("SamsungBrowser")) {
+    // "Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36"
+    return "Samsung Internet";
+  } else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+    // "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 OPR/90.0.4480.54"
+    return "Opera";
+  } else if (userAgent.includes("Edge")) {
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+    return "Microsoft Edge (Legacy)";
+  } else if (userAgent.includes("Edg")) {
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70"
+    return "Microsoft Edge (Chromium)";
+  } else if (userAgent.includes("Chrome")) {
+    // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    return "Google Chrome 或 Chromium";
+  } else if (userAgent.includes("Safari")) {
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1"
+    return "Apple Safari";
+  } else {
+    return "未知";
+  }
 }
-else {
-    alert("没关系，我们会继续努力的！");
-}
-    */
 
   
