3d6aa105cf068d9f0a398319cae5ede8a6ef2d6b
Goth-DongHaiTang <m15178682641@163.com>
2025-07-23 15:24:42 +0800

    m

--- src/main.js
+++ src/main.js
@@ -1,26 +1,2 @@
-import {greetUser} from "./greetUser.js";
-import { getBrowserName } from "./checkUA_bak.js"; // 正确拼写
 
-
-const browserName = getBrowserName(navigator.userAgent);
-const systemName = navigator.platform;
-const name = prompt("请输入你的名字✅：",[""]);
-
-
-if(name=="宁宁"){
-    alert("你已登录！");
-    greetUser(name,browserName, systemName)
-    let bool = confirm("是否跳转到彩蛋界面？")
-    bool ? location.href = "./admin.html" : alert("已返回主页")
-        
-    
-} else if( name.trim() == "" || name==null ||  name==undefined){
-    alert("名字不能为空");
-    location.reload();
-    greetUser(name,browserName, systemName)
-}
-
-else {
-    greetUser(name,browserName, systemName)
-   console.log("不是宁宁吗？🤔");
-}
+setTimeout(() => document.body.style.background = "linear-gradient(135deg, #18b7c2ff , #a23193ff , #10d3a9ff )", 1000); // 恢复回去e
